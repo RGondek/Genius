3965bb7fcf4642585b7d4769e41c3392bf8e3bcf
Patricia Machado de Abreu <Patricia@MacBook-Pro-de-Patricia.local>
2015-02-13 17:22:37 -0200

    Implementado a jogabilidade.

---
+++ ProjConsole/Game.h
@@ -0,0 +1,13 @@
+//
+//  Game.h
+//  ProjConsole
+//
+//  Created by Patricia Machado de Abreu on 13/02/15.
+//  Copyright (c) 2015 Ckode. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Game : NSObject
+
+@end
---
+++ ProjConsole/Game.m
@@ -0,0 +1,13 @@
+//
+//  Game.m
+//  ProjConsole
+//
+//  Created by Patricia Machado de Abreu on 13/02/15.
+//  Copyright (c) 2015 Ckode. All rights reserved.
+//
+
+#import "Game.h"
+
+@implementation Game
+
+@end
