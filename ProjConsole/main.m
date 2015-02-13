7e110a641cc73fe48e69874ca7b93c9304c07ed9
Rubens <rubens.gondek@gmail.com>
2015-02-13 15:22:00 -0200

    Base

---
+++ ProjConsole/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ProjConsole
+//
+//  Created by Rubens Gondek on 2/13/15.
+//  Copyright (c) 2015 Ckode. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
