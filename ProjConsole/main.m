a4ab92a27a015b7faf137a31d738d56e90cca2d9
JoaoMarcos <jmra10@gmail.com>
2015-02-16 14:29:16 -0200

    Implementação do numero da rodada, ajustes no design do menu e em todas as informações dadas ao usuário, e durante o jogo! :)

--- ProjConsole/main.m
+++ ProjConsole/main.m
@@ -7,12 +7,18 @@
 //
 
 
-//Explicação para o uso da estrutura de dados Fila:
-//Como o intuito do jogo é fazer os dados(cores e seus numeros) apresentados ao usuario em um determinado jogo tivesse sempre a mesma ordem independente da jogada, vimos que a estrutura fila é melhor pois o primeiro dado enfileirado é o primeiro dado que será desenfileirado. E com isso garantimos que os dados sigam durante todas as jogadas do jogo a mesma sequencia.
+// Explicação para o uso da estrutura de dados Fila:
+// Como o intuito do jogo é fazer os dados(cores e seus numeros) apresentados ao usuario em um determinado jogo tivesse sempre a mesma ordem independente da jogada, vimos que a estrutura fila é melhor pois o primeiro dado enfileirado é o primeiro dado que será desenfileirado. E com isso garantimos que os dados sigam durante todas as jogadas do jogo a mesma sequencia.
 
 #import <Foundation/Foundation.h>
 #import "User.h"
 #import "Fila.h"
+
+#if __has_feature(objc_arc)
+#define MDLog(format, ...) CFShow((__bridge CFStringRef)[NSString stringWithFormat:format, ## __VA_ARGS__]);
+#else
+#define MDLog(format, ...) CFShow([NSString stringWithFormat:format, ## __VA_ARGS__]);
+#endif
 
 int game(User* usuario)
 {
@@ -20,7 +26,7 @@ int game(User* usuario)
     Fila *fila2 = [[Fila alloc] init];
     NSString *color;
     int dado;
-    int k = 1;
+    int k = 1, numCor = 1;
     
 //    opcoes 1, 2, 3, 4
   
@@ -53,17 +59,17 @@ int game(User* usuario)
             // random só deve ser executado quando i == k-1
             
             
-            NSLog(@"\n%@",[fila1 Ler]);
-            //printf("%@", [fila1 Ler]);
-            [NSThread sleepForTimeInterval:1.5];
-            printf("\n\n\n\n\n\n\n\n\n\n\n");
+            MDLog(@"\n%i. %@", numCor, [fila1 Ler]);
+            [NSThread sleepForTimeInterval:1.0];
+            for (int i = 0; i < 100; i++) printf("\n");
             [fila2 Enfileirar:[fila1 Ler]];
             [fila1 Desenfileirar];
+            numCor++;
         }
         
         for (int i = 0; i < k; i++) {
             scanf("%d", &dado);
-            
+            numCor=1;
             switch (dado) {
                 case 0:
                     color = @"Amarelo(0)";
@@ -85,10 +91,11 @@ int game(User* usuario)
                 [fila2 Desenfileirar];
             }
             else{
+                numCor = 0;
                 return k;
             }
         }
-        printf("\n\n\n\n\n\n\n\n\n\n\n\n");
+        printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
         k++;
     }
 }
@@ -100,8 +107,15 @@ int main(int argc, const char * argv[]) {
         int option, score;
         int qtdJogadas = 0;
         
-        printf("Bem-Vindo, ao MELHOR GENIUS CONSOLE ;D\n\n");
-        printf("(1)Criar Usuário\n(0)SAIR\n");
+        printf("\n");
+        printf("|--------------------------------|\n");
+        printf("|            Bem-Vindo!          |\n");
+        printf("|   Ao MELHOR GENiOS CONSOLE ;D  |\n");
+        printf("|--------------------------------|\n");
+        printf("|       Selecione uma Opção:     |\n");
+        printf("|  (1) Criar Usuário             |\n");
+        printf("|  (0) Sair                      |\n");
+        printf("|--------------------------------|\n\n");
         
         scanf("%i", &option);
         
@@ -109,14 +123,18 @@ int main(int argc, const char * argv[]) {
             switch(option){
                     
                 case 0:{
-                    printf("\n\nObrigado por Jogar, volte sempre!\n\n");
+                    printf("\n\n");
+                    printf("|--------------------------------|\n");
+                    printf("|        Obrigado por Jogar      |\n");
+                    printf("|          Volte sempre!         |\n");
+                    printf("|--------------------------------|\n\n");
                     return 0;
                     break;
                 }
                     
                 case 1:{ // Criar Usuário
                     qtdJogadas = 0;
-                    printf("------------\n| Nome:  | \n------------");
+                    printf("\nNome: ");
                     scanf("%s", aux);
                     NSString *nome = [[NSString alloc] initWithCString:aux encoding:NSASCIIStringEncoding];
                     usuario = [[User alloc] initWithName:nome];
@@ -129,23 +147,32 @@ int main(int argc, const char * argv[]) {
                     printf("\n\n\n\n\n\nVamos Jogar :\n");
                     score = game(usuario);
                     score--;
-                    printf("------------\n| Score: %d | \n------------", score);
-                    printf("\nVocê PERDEEEU!!!\n\n\n LOSER! \n LOSER! \n LOSER! \n\n\n");
+                    printf("\n");
+                    printf("|--------------------------------|\n");
+                    printf("|          Você Perdeu :/        |\n");
+                    printf("| LOSER!  LOSER!  LOSER!  LOSER! |\n");
+                    printf("|--------------------------------|\n\n");
+
+                    printf("|--------------------------------|\n");
+                    printf("|            Score: %d            |\n", score);
+                    printf("|--------------------------------|\n");
                     if (score > usuario.score){
                         usuario.score = score;
                     }
                     break;
                 }
                     
-                case 3:{ // Record
-                    printf("------------\n| Record: %d | \n------------", usuario.score);
+                case 3:{ // Recorde
+                    printf("|--------------------------------|\n");
+                    printf("|         Recorde: %d            |\n", usuario.score);
+                    printf("|--------------------------------|\n");
                     break;
                 }
                     
                 case 4:{ // Trocar de Usuário
                     // Chamar a troca de usuário
                     qtdJogadas = 0;
-                    printf("------------\n| Nome:  | \n------------");
+                    printf("\nNome: ");
                     scanf("%s", aux);
                     NSString *nome = [[NSString alloc] initWithCString:aux encoding:NSASCIIStringEncoding];
                     usuario = [[User alloc] initWithName:nome];
@@ -155,20 +182,38 @@ int main(int argc, const char * argv[]) {
                 }
             
                 case 5: { //Mostrar quantas vezes o usuário jogou
-                    printf("-----------------------------------\n| Quantidade de vezes que jogou: %d |\n-----------------------------------", qtdJogadas);
+                    printf("|--------------------------------|\n");
+                    printf("| Quant. de vezes que jogou: %d  |\n", qtdJogadas);
+                    printf("|--------------------------------|\n");
                     break;
                 }
                 default:{ // Numero errado...
-                    printf("Número Inválido! Digite novamente!\n");
+                    printf("|--------------------------------|\n");
+                    printf("|          Número Inválido       |\n");
+                    printf("|         Digite novamente!      |\n");
+                    printf("|--------------------------------|\n");
                     break;
                 }
             }
             
-            printf("\nSelecione sua opção:\n");
-            printf("(1)Criar Usuário\n(2)Jogar\n(3)Record\n(4)Trocar Usuario\n(5)Quantidade de vezes que jogou\n(0)Sair\n");
+            printf("\n");
+            printf("|--------------------------------|\n");
+            printf("|       Selecione uma Opção:     |\n");
+            printf("|  (1) Criar Usuário             |\n");
+            printf("|  (2) Jogar                     |\n");
+            printf("|  (3) Recorde                   |\n");
+            printf("|  (4) Trocar Usuário            |\n");
+            printf("|  (5) Quant. de vezes que jogou |\n");
+            printf("|  (0) Sair                      |\n");
+            printf("|--------------------------------|\n\n");
+            
             scanf("%i", &option);
         }
-        printf("\n\nObrigado por Jogar, volte sempre!\n\n");
+        printf("\n\n");
+        printf("|--------------------------------|\n");
+        printf("|        Obrigado por Jogar      |\n");
+        printf("|          Volte sempre!         |\n");
+        printf("|--------------------------------|\n\n");
     }
     return 0;
 }
